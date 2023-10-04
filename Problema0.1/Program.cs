cc517ff4f45d62b66f595deaa1632ba351a7abd1
USER <USER@DESKTOP-KCU37G9>
2023-10-04 15:01:11 -0500

    Agregar archivos de proyecto.

---
+++ Problema0.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema0._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine("Divisor UPN " + (i / 3) + ": " + i);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
